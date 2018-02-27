e62c71bf47174678b07f63740535d16ff85ba91c
akeco <amerkecho@hotmail.com>
2018-02-28 00:25:05 +0100

    Prepare for heroku

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 const {tokenValidation} = require('../controlers/oauth');
 const login_controller = require('../controlers/login_controller');
 
@@ -11,6 +12,24 @@ router.get('/', function(req, res, next) {
 
 router.get('/oauth', tokenValidation, function(req, res, next){
     res.sendStatus(200);
+});
+
+router.get('/email-validation/:randomID', async (req, res, next)=>{
+    try{
+        const result = await axios.get(`http://localhost:3000/api/UserModels?filter[where][emailToken]=${req.params.randomID}`);
+        if(result.status == 200 && result.data && result.data.length){
+                const patchResult = await axios.patch(`http://localhost:3000/api/UserModels/${result.data[0].id}`,{
+                    emailVerified: true,
+                    emailToken: null
+                });
+                if(patchResult.status == 200 && patchResult.data){
+                    res.send(`Email ${result.data[0].email} je verifikovan. Možete pristupiti mobilnoj aplikaciji!`);
+                }
+        }
+    }
+    catch(e){
+        console.info("AJAX request error", e);
+    }
 });
 
 router.get("*", function (req, res, next) {
